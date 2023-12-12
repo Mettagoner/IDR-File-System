d23d1db26b192dceb4416ca2b25fe11578f02b80
Daniel F <danfdev33@gmail.com>
2023-12-11 21:52:34 -0500

    Revamped DiskLib directory by adding bitmap for management of disk blocks.

--- DiskLib/disklib.c
+++ DiskLib/disklib.c
@@ -1,49 +1,104 @@
 /********************************************************************************
- * Filename:     disklib.c
- * Author:       Daniel Fewell
- * Last Edited:  12/11/2023
- * Description:  Implementation of the 3 functions declared in disklib.h
- * ********************************************************************************/
+ * Filename: disklib.c
+ * Author: Daniel Fewell
+ * Last Edited: 12/11/2023
+ * Description: Implementation of the 8 functions declared in disklib.h
+ ********************************************************************************/
 
 #include "disklib.h"
-
+#include <limits.h> // For CHAR_BIT
 
 // Global variable for the disk file
 FILE *disk = NULL;
+unsigned char bitmap[BITMAP_SIZE]; // Bitmap array
 
-// Initialize the virtual disk
+
+
+// Initialize the virtual disk:
 void init_disk() {
-	disk = fopen("virtual_disk.img", "r+b"); // Call the 'fopen' function in C standard lib to open file named 'virtual_disk.img' for reading & writing in binary mode
-	if disk = NULL {
-	// Create file if it does not already exist
-	disk = fopen("virtual_disk.img", "w+b"); // Open file for writing & reading in binary mode
-	if (disk == NULL) {
-		perror("Error Code 1: Unable to reate virtual disk"); // Print Error Code #1: Unable to create virtual disk
-		exit(1);
-	}
-	fseek(disk, BLOCK_SIZE * NUM_BLOCKS -1, SEEK_SET); // Move the file pointer to the last byte of the calculated size 
-	fputc(0, disk); // Writes a single char to the current file pointer position. 0 is the char byte being written. This ensures the file is extended or reduced to our specified 5MB size 
-	}
+    disk = fopen("virtual_disk.img", "r+b");
+    if (disk == NULL) {
+        disk = fopen("virtual_disk.img", "w+b");
+        if (disk == NULL) {
+            perror("Error Code 1: Unable to create virtual disk");
+            exit(1);
+        }
+        // Initialize disk to specified size and create bitmap
+        fseek(disk, BLOCK_SIZE * NUM_BLOCKS - 1, SEEK_SET);
+        fputc(0, disk);
+        memset(bitmap, 0, BITMAP_SIZE); // Initialize all blocks as free
+        save_bitmap(); // Save the initial bitmap to disk
+    } else {
+        init_bitmap(); // Load bitmap from disk
+    }
 }
 
-//Read a block from the disk into a buffer
+
+
+// Bitmap Functions
+void init_bitmap() {
+    fseek(disk, 0, SEEK_SET);// Set the file position to the beginning of the disk file
+    size_t bytesRead = fread(bitmap, sizeof(char), BITMAP_SIZE, disk); // Read the bitmap data from the disk into the bitmap array
+    if (bytesRead != BITMAP_SIZE) { // Check if the number of bytes read is equal to the expected bitmap size
+        perror("Error reading bitmap from disk"); // If not, output an error message and exit the program
+        exit(1);
+    }
+}
+
+void save_bitmap() {
+    fseek(disk, 0, SEEK_SET);// Set the file position to the beginning of the disk file where the bitmap is stored
+    size_t bytesWritten = fwrite(bitmap, sizeof(char), BITMAP_SIZE, disk); // Write the bitmap data from the bitmap array to the disk
+    if (bytesWritten != BITMAP_SIZE) { // Check if the number of bytes written is equal to the expected bitmap size
+        perror("Error writing bitmap to disk"); // If not, output an error message and exit the program
+        exit(1);
+    }
+    fflush(disk); // Flush the output buffer to ensure all data is written to the disk
+}
+
+int find_free_block() {
+    for (int i = 0; i < BITMAP_SIZE * CHAR_BIT; ++i) { // Iterate through each bit in the bitmap
+        int byteIndex = i / CHAR_BIT; // Calculate the byte index in the bitmap array
+        int bitIndex = i % CHAR_BIT; // Calculate the bit index within the byte
+        if (!(bitmap[byteIndex] & (1 << bitIndex))) { // Check if the bit is 0 (free)
+            return i; // Return the index of the free block
+        }
+    }
+    return -1; // No free block found
+}
+
+void mark_block_used(int blockIndex) {
+    int byteIndex = blockIndex / CHAR_BIT; // Calculate the byte index in the bitmap array
+    int bitIndex = blockIndex % CHAR_BIT; // Calculate the bit index within the byte
+    bitmap[byteIndex] |= (1 << bitIndex); // Set the bit at blockIndex to 1 (used)
+    save_bitmap(); // Save the updated bitmap to the disk
+}
+
+void mark_block_free(int blockIndex) {
+    int byteIndex = blockIndex / CHAR_BIT; // Calculate the byte index in the bitmap array
+    int bitIndex = blockIndex % CHAR_BIT; // Calculate the bit index within the byte
+    bitmap[byteIndex] &= ~(1 << bitIndex); // Set the bit at blockIndex to 0 (free)
+    save_bitmap(); // Save the updated bitmap to the disk
+}
+
+
+
+// Read_block, write_block functions here:
 int read_block(int block_num, void *block) { // void* block allows us to pass a pointer to a buffer of any data type
-	if (block_num < 0 || block_num >= NUM_BLOCKS) { // If conditions are met, return invalid block number
-		return -1; // Return failure upon completion
-	}
+        if (block_num < 0 || block_num >= NUM_BLOCKS) { // If conditions are met, return invalid block number
+                return -1; // Return failure upon completion
+        }
 
-	fseek(disk, block_num * BLOCK_SIZE, SEEK_SET); // Position the file pointer
-	fread(block, BLOCK_SIZE, 1, disk); // Use fread function to read data into the buffer (block)
-	return 0; // Return success upon completion
+        fseek(disk, block_num * BLOCK_SIZE, SEEK_SET); // Position the file pointer
+        fread(block, BLOCK_SIZE, 1, disk); // Use fread function to read data into the buffer (block)
+        return 0; // Return success upon completion
 }
 
-//Write a block to the virtual disk
 int write_block(int block_num, const void *block) {
-	if (block_num < 0 || block_num >= NUM_BLOCKS) { // If conditions are met, return invalid block number
-		return -1; // Return failure upon completion
-	}
+        if (block_num < 0 || block_num >= NUM_BLOCKS) { // If conditions are met, return invalid block number
+                return -1; // Return failure upon completion
+        }
 
-	fseek(disk, block_num * BLOCK_SIZE, SEEK_SET); // Position the file pointer
-	fwrite(block, BLOCK_SIZE, 1, disk); // Use fwrite function to write data to the position of the file pointer
-	return 0; // Return success upon completion
+        fseek(disk, block_num * BLOCK_SIZE, SEEK_SET); // Position the file pointer
+        fwrite(block, BLOCK_SIZE, 1, disk); // Use fwrite function to write data to the position of the file pointer
+        return 0; // Return success upon completion
 }
--- DiskLib/disklib.h
+++ DiskLib/disklib.h
@@ -2,7 +2,7 @@
  * Filename:     disklib.h
  * Author:       Daniel Fewell
  * Last Edited:  12/11/2023
- * Description:  Header File for the 3 functions defined in disklib.c
+ * Description:  Header File for the 8 functions defined in disklib.c
  * ********************************************************************************/
 
 // Include Guards (prevents multiple inclusions and redefinition errors)
@@ -15,10 +15,16 @@
 // Constants
 #define BLOCK_SIZE 4096 // Set the size of each block in bytes
 #define NUM_BLOCKS 1280 // Set the number of blocks in the disk (5MB total size)
+#define BITMAP_SIZE (NUM_BLOCKS / 8) // Size of bitmap in bytes
 
 // Function declarations
-void init_disk();
-int read_block(int block_num, void *block);
-int write_block(int block_num, const void *block);
+void init_disk(); // Inits disk and bitmap
+int read_block(int block_num, void *block); // Reads a block from the disk
+int write_block(int block_num, const void *block); // Writes a block to the disk
+void init_bitmap(); // Init bitmap from the disk
+void save_bitmap(); // Save bitmap to the disk
+int find_free_block(); // Find the first free block using the bitmap
+void mark_block_used(int blockIndex); // Marks a block as used in the bitmap
+void mark_block_free(int blockIndex); // Marks a block as free in the bitmap
 
 #endif
